9bdf8328406d5ec322cc78889549a1d8942a4f71
Zhenya Loyko <e.lojko@inbox.ru>
2023-08-23 19:08:50 +0300

    Some code

---
+++ SignInUp App/SignInUp App/Controllers/CreateAccountVC.swift
@@ -0,0 +1,80 @@
+//
+//  CreateAccountVC.swift
+//  SignInUp App
+//
+//  Created by Евгений Лойко on 23.08.23.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var errorEmailLbl: UILabel!
+    
+    @IBOutlet weak var nameTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var errorPasswordLbl: UILabel!
+    
+    @IBOutlet var passwordIndicatorsViews: [UIView]!
+    
+    @IBOutlet weak var confirmPasswordTF: UITextField!
+    @IBOutlet weak var errorPasswordConfirmLbl: UILabel!
+    
+    @IBOutlet weak var continueBtn: UIButton!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        errorEmailLbl.isHidden = true
+        errorPasswordLbl.isHidden = true
+        passwordIndicatorsViews.forEach { view in
+            view.alpha = 0.2
+        }
+        errorPasswordConfirmLbl.isHidden = true
+        continueBtn.isEnabled = false
+        
+        startKeyboardObserver()
+    }
+
+    
+    
+    
+    
+    
+    
+    private func startKeyboardObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(notification: Notification) {
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc private func keyboardWillHide() {
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
