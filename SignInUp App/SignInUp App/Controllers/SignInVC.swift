073026b45ea44d3296f8a2bcf64827608554c721
Zhenya Loyko <e.lojko@inbox.ru>
2023-08-23 18:04:55 +0300

    SignUpStoryboard designed

---
+++ SignInUp App/SignInUp App/Controllers/SignInVC.swift
@@ -0,0 +1,37 @@
+//
+//  SignInVC.swift
+//  SignInUp App
+//
+//  Created by Евгений Лойко on 23.08.23.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var errorLbl: UILabel!
+    @IBOutlet weak var signInBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        errorLbl.isHidden = true
+        signInBtn.isEnabled = false
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
